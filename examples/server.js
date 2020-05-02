1b5af6f4bdfca939e780ae70bd6e1819425824f7
Jannis R <mail@jannisr.de>
2020-05-02 15:32:36 +0200

    move code, server example :memo:

---
+++ examples/server.js
@@ -0,0 +1,37 @@
+'use strict'
+
+const createCert = require('create-cert')
+const {
+	createServer: createGeminiServer,
+	DEFAULT_PORT,
+} = require('..')
+
+const onRequest = (req, res) => {
+	if (req.path === '/foo') {
+		res.write('foo,')
+		res.end('bar')
+	} else {
+		res.gone()
+	}
+}
+
+const onError = (err) => {
+	console.error(err)
+	process.exit(1)
+}
+
+createCert('example.org')
+.then((keys) => {
+	const server = createGeminiServer({
+		tlsOpt: keys,
+		// todo: SNICallback
+	}, onRequest)
+	server.on('error', onError)
+
+	server.listen(DEFAULT_PORT, (err) => {
+		if (err) return onError(err)
+		const {address, port} = server.address()
+		console.info(`listening on ${address}:${port}`)
+	})
+})
+.catch(onError)
