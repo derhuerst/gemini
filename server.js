f429425bc76f89a9776adca69a8637311eca0294
Martijn de Boer <github@sexybiggetje.nl>
2020-10-15 15:30:25 +0200

    Remove ALPN protocols for issue #5
    When setting the ALPN protocol the gemini browser Kristall won't connect properly. While removing this, per suggestion in #5, connection seems to be working properly.

--- server.js
+++ server.js
@@ -85,7 +85,6 @@ const createGeminiServer = (opt = {}, onRequest) => {
 	}
 
 	const server = createTlsServer({
-		ALPNProtocols: [ALPN_ID],
 		minVersion: MIN_TLS_VERSION,
 		// > Usually the server specifies in the Server Hello message if a
 		// > client certificate is needed/wanted.
